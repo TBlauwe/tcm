3d75d7e63051ab61fdd391ed0352c3f0edb09a55
TBlauwe <tdeblauwe@pm.me>
2024-12-19 20:24:06 +0100

    Fix ispc header suffix

--- src/11_ISPC.cmake
+++ src/11_ISPC.cmake
@@ -2,21 +2,22 @@
 # --- ISPC
 # ------------------------------------------------------------------------------
 
-function(tcm_target_setup_ispc)
+function(tcm_target_setup_ispc target)
     set(options)
     set(oneValueArgs
             HEADER_DIR
             HEADER_SUFFIX
             INSTRUCTION_SETS
     )
-    cmake_parse_arguments(PARSE_ARGV 0 arg "${options}" "${oneValueArgs}" "${multiValueArgs}")
+    set(multiValueArgs)
+    cmake_parse_arguments(PARSE_ARGV 1 arg "${options}" "${oneValueArgs}" "${multiValueArgs}")
 
-    if(arg_HEADER_DIR)
-        set_target_properties(ispc_lib PROPERTIES ISPC_HEADER_DIRECTORY ${arg_HEADER_DIR})
-    endif ()
-    if(arg_HEADER_SUFFIX)
-        set_target_properties(ispc_lib PROPERTIES ISPC_HEADER_SUFFIX ${arg_HEADER_SUFFIX})
-    endif ()
+    tcm__default_value(arg_HEADER_DIR "${CMAKE_CURRENT_BINARY_DIR}/ispc/")
+    tcm__default_value(arg_HEADER_SUFFIX ".h")
+
+    set_target_properties(ispc_lib PROPERTIES ISPC_HEADER_DIRECTORY ${arg_HEADER_DIR})
+    set_target_properties(ispc_lib PROPERTIES ISPC_HEADER_SUFFIX ${arg_HEADER_SUFFIX})
+
     if(arg_INSTRUCTION_SETS)
         set_target_properties(ispc_lib PROPERTIES ISPC_INSTRUCTION_SETS ${arg_INSTRUCTION_SETS})
     endif ()
