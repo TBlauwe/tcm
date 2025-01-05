254096a18a3ae15f33c06ec546ed449fbb0b9b62
TBlauwe <tdeblauwe@pm.me>
2025-01-05 20:31:13 +0100

    Fix case where only static library is built.

--- cmake/tcm.cmake
+++ cmake/tcm.cmake
@@ -712,20 +712,22 @@ function(tcm_target_export_header arg_TARGET)
     # Generate export header, even for static library as they need the header to compile
     target_include_directories(${arg_TARGET} SYSTEM PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/export>)
 
+    if(NOT EXISTS ${arg_EXPORT_FILE_NAME})
+        # Generate export header, even for static library as they need the header to compile
+        generate_export_header(
+                ${arg_TARGET}
+                BASE_NAME ${arg_BASE_NAME}
+                EXPORT_FILE_NAME ${arg_EXPORT_FILE_NAME}
+                EXPORT_MACRO_NAME ${arg_BASE_NAME_UPPER}_API
+        )
+    endif ()
+
     # Check type instead of BUILD_SHARED_LIBS as a library type could be forced.
     get_target_property(target_type ${arg_TARGET} TYPE)
     if (target_type STREQUAL "STATIC_LIBRARY")
         target_compile_definitions(${arg_TARGET} PUBLIC ${arg_BASE_NAME_UPPER}_STATIC_DEFINE)
         return() # The rest of the function is relevant only for a shared library
     endif ()
-
-    # Generate export header, even for static library as they need the header to compile
-    generate_export_header(
-            ${arg_TARGET}
-            BASE_NAME ${arg_BASE_NAME}
-            EXPORT_FILE_NAME ${arg_EXPORT_FILE_NAME}
-            EXPORT_MACRO_NAME ${arg_BASE_NAME_UPPER}_API
-    )
 
     set_target_properties(${arg_TARGET} PROPERTIES
             CXX_VISIBILITY_PRESET hidden
